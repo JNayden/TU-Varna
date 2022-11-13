b68c1b9f9c1951be3ca25348eb639ab0019c8b77
JNayden <56478030+JNayden@users.noreply.github.com>
2022-11-14 01:29:44 +0200

    Create TokenType.java

---
+++ EP/28:1-5/TokenType.java
@@ -0,0 +1,19 @@
+private static Set<String> keywords = new HashSet<>();
+    static{
+        keywords.add(IF.value);
+        /* ToDo - Add all other keywords into the HashSet */
+
+        keywords.add(INT.value);
+        keywords.add(CHAR.value);
+        keywords.add(ELSE.value);
+        keywords.add(READ.value);
+        keywords.add(TRUE.value);
+        keywords.add(VOID.value);
+        keywords.add(PROGRAM.value);
+        keywords.add(FALSE.value);
+        keywords.add(PRINT.value);
+        keywords.add(WHILE.value);
+        keywords.add(LENGTH.value);
+        keywords.add(RETURN.value);
+        keywords.add(BOOLEAN.value);
+    }
